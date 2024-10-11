47af9a77946dfe87144d85853299fcb5d30826ad
fernandajuliettepasillasgonzalez <fernanda_pasillas24@unikino.edu.mx>
2024-10-11 16:31:08 -0600

    Add project files.

---
+++ ProyectoEvaluacionFJPG/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ProyectoEvaluacionFJPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Estudiante! Por favor Ingresa tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu calificacion: ");
+            decimal calificacion = decimal.Parse(Console.ReadLine());
+
+            if (calificacion <= 50)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Estudiante {nombre}, calificacion {calificacion}. Reprobado.");
+            }
+            else if (calificacion >= 80)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Estudiante {nombre}, calificacion {calificacion}. Aprobado.");
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow; 
+                Console.WriteLine($"Estudiante {nombre}, calificacion {calificacion}. Reprobado con derecho a recursar.");
+
+            }
+        }
+    }
+}
